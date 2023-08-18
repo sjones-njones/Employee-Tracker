c895317e8db3f09b0b2385c9430c2e900e378ace
stephanie jones <sjones.njones@gmail.com>
2023-08-18 16:51:26 -0500

    added 2nd option if statement

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,9 +5,19 @@ VALUES  ('Math'),
         ('History'),
         ('Art');
 
--- INSERT INTO role (title, salary, department_id)
--- VALUES ('Teacher, 36000.00, 1'),
--- ('Teacher, 36000.00, 2'),('Teacher, 36000.00, 3'),('Teacher, 36000.00, 4'),('Teacher, 36000.00, 5'),('Teacher Assistant, 26000.00, 3'),('Teacher Assistant, 26000.00, 5'), ('Teacher Assistant, 26000.00, 3');
+INSERT INTO role (title, salary, department_id)
+VALUES  ('Teacher, 36000.00, 1'),
+        ('Teacher, 36000.00, 2'),
+        ('Teacher, 36000.00, 3'),
+        ('Teacher, 36000.00, 4'),
+        ('Teacher, 36000.00, 5'),
+        ('Teacher Assistant, 26000.00, 3'),('Teacher Assistant, 26000.00, 5'), ('Teacher Assistant, 26000.00, 3');
 
--- INSERT INTO employee (first_name, last_name, role_id, manager_id)
--- VALUES ('Stephanie', 'Jones', 1, 12), ('Neil', 'Jones', 2, 13),('Maya', 'Jones', 3, 14), ('Rosie', 'Jones', 4, 15), ('Susan', 'Hodgeson', 5, 16), ('Roy', 'Hodgeson', 6, ), ('Holden', 'Hodgeson', 7, 18), ('Weston', 'Hodgeson', 8, 19);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Stephanie', 'Jones', 1, 1),         
+        ('Neil', 'Jones', 2, 2),
+        ('Maya', 'Jones', 3, 3), 
+        ('Rosie', 'Jones', 4, 4), 
+        ('Susan', 'Hodgeson', 5, 5), 
+        ('Roy', 'Hodgeson', 6, 6), 
+        ('Holden', 'Hodgeson', 7, 7);
